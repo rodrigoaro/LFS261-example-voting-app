7e6dea806abb00055d15eca2737d0c28097c07f1
RODRIGO ALEXIS ARO RINGELE <rarorin@contraloria.cl>
2024-03-26 16:43:25 -0300

    Changes 2.

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 
 option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_b = os.getenv('OPTION_B', "Dogss")
 hostname = socket.gethostname()
 version = 'v1'
 
